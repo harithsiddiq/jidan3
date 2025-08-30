59133b07d2c1c5709186eceb4e1f2646a704abd7
taba2177 <tabadev217@gmail.com>
2025-08-30 16:19:02 +0300

    #

--- laravel/tailwind.config.js
+++ laravel/tailwind.config.js
@@ -7,6 +7,6 @@ import defaultTheme from 'tailwindcss/defaultTheme';
          "./resources/views/filament/**/*.blade.php",
          "./resources/views/**/*.blade.php",
          "./vendor/filament/**/*.blade.php",
-         "./packages/taba/crm/resources/views/**/*.blade.php", // Add crm views
+         "./vendor/taba/crm/resources/views/**/*.blade.php", // Add crm views
      ],
  };
