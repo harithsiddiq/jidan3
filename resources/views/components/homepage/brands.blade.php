37cd4ed84f3776f2a1edb36749e113dea26f74de
taba2177 <tabadev217@gmail.com>
2025-08-30 22:44:41 +0300

    #

--- resources/views/components/homepage/brands.blade.php
+++ resources/views/components/homepage/brands.blade.php
@@ -1,3 +1,5 @@
+        @props(['posts'])
+        @if(!empty($posts))
         <!-- start: Brand Marque Area -->
         <div class=" pt-60px md:pt-20 lg:pt-30 overflow-x-hidden wow fadeInUp " data-wow-delay=".3s">
 
@@ -7,104 +9,26 @@
                     class="text-size-13 sm:text-base font-semibold uppercase inline-block px-6 bg-white-color dark:bg-primary-color-light text-seondary-color dark:text-white-color">
 
                     <span class="text-primary-color">
-                        100+
-                    </span> Trusted Clients Over the world
+                        {{ $posts->first()->postCategory->name}}+
+                    </span>
+                    {{ $posts->first()->postCategory->subtitle}}
                 </p>
             </div>
             <div class="tj-roll-section ">
                 <div class="relative z-0 mask-fade-horizontal-2">
                     <div class="tj-marquee tj-marquee--3 swiper  ">
                         <div class="swiper-wrapper">
+                            @foreach($posts as $index => $post)
+
                             <div
                                 class="swiper-slide cursor-pointer max-w-full !w-[200px] h-[84px] rounded-10px bg-cream-light-color dark:bg-black-color flex items-center justify-center relative z-0 overflow-hidden after:absolute after:left-0 after:right-0 after:w-full after:h-full after:-z-1 after:opacity-0 after:transition-all after:duration-300 after:bg-200 after:bg-gradient-secondary hover:after:opacity-100 group">
-
                                 <div class="max-w-100px md:max-w-full">
                                     <img class="invert dark:invert-0 w-full transition-all duration-300 group-hover:brightness-100  group-hover:invert-0"
-                                        src="{{ asset("/./assets/img/roll/roll-img1.png") }}" alt="Brand" />
-
-
+                                        src="{{ asset($post->image?->url ?? $post->getRandomImage()) }}"
+                                        alt="{{ $post->image?->alt ?? "brand image"}}" />
                                 </div>
-
                             </div>
-                            <div
-                                class="swiper-slide cursor-pointer max-w-full !w-[200px] h-[84px] rounded-10px bg-cream-light-color dark:bg-black-color flex items-center justify-center relative z-0 overflow-hidden after:absolute after:left-0 after:right-0 after:w-full after:h-full after:-z-1 after:opacity-0 after:transition-all after:duration-300 after:bg-200 after:bg-gradient-secondary hover:after:opacity-100 group">
-
-                                <div class="max-w-100px md:max-w-full">
-                                    <img class="invert dark:invert-0 w-full transition-all duration-300 group-hover:brightness-100  group-hover:invert-0"
-                                        src="{{ asset("/./assets/img/roll/roll-img2.png") }}" alt="Brand" />
-
-
-                                </div>
-
-                            </div>
-                            <div
-                                class="swiper-slide cursor-pointer max-w-full !w-[200px] h-[84px] rounded-10px bg-cream-light-color dark:bg-black-color flex items-center justify-center relative z-0 overflow-hidden after:absolute after:left-0 after:right-0 after:w-full after:h-full after:-z-1 after:opacity-0 after:transition-all after:duration-300 after:bg-200 after:bg-gradient-secondary hover:after:opacity-100 group">
-
-                                <div class="max-w-100px md:max-w-full">
-                                    <img class="invert dark:invert-0 w-full transition-all duration-300 group-hover:brightness-100  group-hover:invert-0"
-                                        src="{{ asset("/./assets/img/roll/roll-img3.png") }}" alt="Brand" />
-
-
-                                </div>
-
-                            </div>
-                            <div
-                                class="swiper-slide cursor-pointer max-w-full !w-[200px] h-[84px] rounded-10px bg-cream-light-color dark:bg-black-color flex items-center justify-center relative z-0 overflow-hidden after:absolute after:left-0 after:right-0 after:w-full after:h-full after:-z-1 after:opacity-0 after:transition-all after:duration-300 after:bg-200 after:bg-gradient-secondary hover:after:opacity-100 group">
-
-                                <div class="max-w-100px md:max-w-full">
-                                    <img class="invert dark:invert-0 w-full transition-all duration-300 group-hover:brightness-100  group-hover:invert-0"
-                                        src="{{ asset("/./assets/img/roll/roll-img4.png") }}" alt="Brand" />
-
-
-                                </div>
-
-                            </div>
-                            <div
-                                class="swiper-slide cursor-pointer max-w-full !w-[200px] h-[84px] rounded-10px bg-cream-light-color dark:bg-black-color flex items-center justify-center relative z-0 overflow-hidden after:absolute after:left-0 after:right-0 after:w-full after:h-full after:-z-1 after:opacity-0 after:transition-all after:duration-300 after:bg-200 after:bg-gradient-secondary hover:after:opacity-100 group">
-
-                                <div class="max-w-100px md:max-w-full">
-                                    <img class="invert dark:invert-0 w-full transition-all duration-300 group-hover:brightness-100  group-hover:invert-0"
-                                        src="{{ asset("/./assets/img/roll/roll-img5.png") }}" alt="Brand" />
-
-
-                                </div>
-
-                            </div>
-                            <div
-                                class="swiper-slide cursor-pointer max-w-full !w-[200px] h-[84px] rounded-10px bg-cream-light-color dark:bg-black-color flex items-center justify-center relative z-0 overflow-hidden after:absolute after:left-0 after:right-0 after:w-full after:h-full after:-z-1 after:opacity-0 after:transition-all after:duration-300 after:bg-200 after:bg-gradient-secondary hover:after:opacity-100 group">
-
-                                <div class="max-w-100px md:max-w-full">
-                                    <img class="invert dark:invert-0 w-full transition-all duration-300 group-hover:brightness-100  group-hover:invert-0"
-                                        src="{{ asset("/./assets/img/roll/roll-img6.png") }}" alt="Brand" />
-
-
-                                </div>
-
-                            </div>
-                            <div
-                                class="swiper-slide cursor-pointer max-w-full !w-[200px] h-[84px] rounded-10px bg-cream-light-color dark:bg-black-color flex items-center justify-center relative z-0 overflow-hidden after:absolute after:left-0 after:right-0 after:w-full after:h-full after:-z-1 after:opacity-0 after:transition-all after:duration-300 after:bg-200 after:bg-gradient-secondary hover:after:opacity-100 group">
-
-                                <div class="max-w-100px md:max-w-full">
-                                    <img class="invert dark:invert-0 w-full transition-all duration-300 group-hover:brightness-100  group-hover:invert-0"
-                                        src="{{ asset("/./assets/img/roll/roll-img7.png") }}" alt="Brand" />
-
-
-                                </div>
-
-                            </div>
-                            <div
-                                class="swiper-slide cursor-pointer max-w-full !w-[200px] h-[84px] rounded-10px bg-cream-light-color dark:bg-black-color flex items-center justify-center relative z-0 overflow-hidden after:absolute after:left-0 after:right-0 after:w-full after:h-full after:-z-1 after:opacity-0 after:transition-all after:duration-300 after:bg-200 after:bg-gradient-secondary hover:after:opacity-100 group">
-
-                                <div class="max-w-100px md:max-w-full">
-                                    <img class="invert dark:invert-0 w-full transition-all duration-300 group-hover:brightness-100  group-hover:invert-0"
-                                        src="{{ asset("/./assets/img/roll/roll-img8.png") }}" alt="Brand" />
-
-
-                                </div>
-
-                            </div>
-
-
+                            @endforeach
                         </div>
                     </div>
                 </div>
