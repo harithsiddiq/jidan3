0e9711630a98524fd8e7eeb45cc78d6f583ecb90
taba2177 <tabadev217@gmail.com>
2025-08-30 15:11:15 +0300

    #

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "vendor/taba/crm/src/resources/css/admin.css",
+            ],
             refresh: true,
         }),
         tailwindcss(),
