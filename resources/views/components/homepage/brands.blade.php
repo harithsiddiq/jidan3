bd7e9480010dfd74639f5f866c830008f7552431
taba <taba217@hotmail.com>
2025-08-31 03:28:45 +0300

    #

--- resources/views/components/homepage/brands.blade.php
+++ resources/views/components/homepage/brands.blade.php
@@ -1,10 +1,10 @@
         @props(['posts'])
         @if(!empty($posts))
         <!-- start: Brand Marque Area -->
-        <div class="pt-60px md:pt-20 lg:pt-30 overflow-x-hidden wow fadeInUp " data-wow-delay=".3s">
+        <div class="dark pt-60px md:pt-20 lg:pt-30 overflow-x-hidden wow fadeInUp " data-wow-delay=".3s">
 
             <div
-                class="text-center mb-50px md:mb-60px after:w-full after:h-1px after:bg-body-color dark:after:bg-dark-color-2 after:absolute after:top-1/2 after:left-0 after:-translate-y-1/2 relative z-0 after:-z-1 ">
+                class="dark text-center mb-50px md:mb-60px after:w-full after:h-1px after:bg-body-color dark:after:bg-dark-color-2 after:absolute after:top-1/2 after:left-0 after:-translate-y-1/2 relative z-0 after:-z-1 ">
                 <p
                     class="text-size-13 sm:text-base font-semibold uppercase inline-block px-6 bg-white-color dark:bg-primary-color-light text-seondary-color dark:text-white-color">
 
