de29b1beeaf48067555ccabca63e1205bdc0ea4e
taba <taba217@hotmail.com>
2025-08-31 03:34:19 +0300

    #

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -36,7 +36,7 @@
 
 <body class="font-sora dark:bg-dark-color relative">
     <!-- Preloader Area Start -->
-    <div class="preloader">
+    <div class="preloader dark">
         <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
             <path id="preloaderSvg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
         </svg>
